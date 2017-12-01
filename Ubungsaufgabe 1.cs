8197692957cc94b56845424deca44c28a5a6a7cf
A.Bergner <A.Bergner@kbbz.ads>
2017-12-01 10:57:34 +0100

    Neue Aufgabe Hinzugefügt

---
+++ Ubungsaufgabe 1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ubungsaufgabe1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] freunde = new string[] { "Stefan", "Kevin", "Manfred" };
+
+            printArray(freunde);
+
+            // überschreibt wert mit leerem string >> größe bleibt
+            freunde[1] = string.Empty;
+
+            printArray(freunde);
+
+            // fügt Uschi in die leere Stelle
+            freunde[2] = "Uschi";
+
+            printArray(freunde);
+
+            // erstellt neues array >> lässt einen wert aus >> verkleinert
+            // freunde = new string[] {freunde[0], freunde[2]};
+
+            // erstellt neues array >> fügt Uschi hinten an >> vergrößert
+            // freunde = new string[] {freunde[0], freunde[1], "Uschi"};
+
+            //printArray(freunde);
+
+        }
+
+        private static void printArray(string[] array)
+        {
+            foreach (string element in array)
+            {
+                Console.WriteLine(element + " ");
+            }
+        }
+    }
+}
