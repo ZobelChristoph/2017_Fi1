facb55bf6515ab7c9dd785807d970a864c38994f
F.Kolb <F.Kolb@kbbz.ads>
2017-12-01 10:13:48 +0100

    Absichtlicher Konflikt

--- Testaufgabe/Testaufgabe/Program.cs
+++ Testaufgabe/Testaufgabe/Program.cs
@@ -7,6 +7,7 @@ namespace Testaufgabe
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+			Console.WriteLine("Hello World2!");
             Console.ReadLine();
         }
     }
