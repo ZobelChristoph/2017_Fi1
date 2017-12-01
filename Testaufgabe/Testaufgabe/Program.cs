db261fbaabb3312aa85f835d85a6046edfb613f4
MarcoGran <M.Gransitzki@kbbz.ads>
2017-12-01 11:11:32 +0100

    *

--- Testaufgabe/Testaufgabe/Program.cs
+++ Testaufgabe/Testaufgabe/Program.cs
@@ -6,7 +6,7 @@ namespace Testaufgabe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World1!");
 			Console.WriteLine("Hello World2!");
 Console.WriteLine("Hello World3!");
             Console.ReadLine();
