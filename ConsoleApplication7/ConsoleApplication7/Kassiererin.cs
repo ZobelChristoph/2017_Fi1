8493ad976ada587da3e7bc7ee1f93af6306aa836
Zobel <c.zobel@gmx.net>
2017-12-08 12:17:06 +0100

    Mehrdim

---
+++ ConsoleApplication7/ConsoleApplication7/Kassiererin.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication7
+{
+    public class Kassiererin
+    {
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// 
+        public Kassiererin()
+        {
+
+        }
+
+        public Kassiererin(Kinosaal kinosaal)
+        {
+
+        }
+
+        public void begruesseKunden()
+        {
+            Console.WriteLine("Willkommen im Kino!\n\n");
+        }
+
+        public void verabschiedeKunden()
+        {
+            Console.WriteLine("\n\nAuf Wiedersehen!");
+        }
+
+        public void bestaetigeReservierung(int reihe, int sitz)
+        {
+            Console.WriteLine("Der Platz R" + reihe + "-S" + sitz.ToString() + " ist jetzt für Sie reserviert.");
+        }
+
+        public void lehneReservierungAb(int reihe, int sitz)
+        {
+            Console.WriteLine("Der Platz R" + reihe.ToString() + "-S" + sitz.ToString() + " ist leider schon belegt.");
+        }
+
+        public void frageNachFortsetzung()
+        {
+            Console.WriteLine("Möchten Sie fortfahren? Ja: ENTER | Nein: SPACE");
+        }
+
+        public int frageNachReihe()
+        {
+            Console.WriteLine("In welcher Reihe möchten Sie sitzen? (1-10)");
+            return int.Parse(Console.ReadLine());
+        }
+
+        public int frageNachSitz()
+        {
+            Console.WriteLine("In welchem Platz möchten Sie sitzen? (1-20)");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ ConsoleApplication7/ConsoleApplication7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication7
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Kinosaal kinosaal = new Kinosaal(10, 20);
+            Kassiererin kassiererin= new Kassiererin();
+            
+
+            kassiererin.begruesseKunden();
+            neueReservierung(kinosaal, kassiererin);
+
+        }
+
+        private static void neueReservierung(Kinosaal kinosaal, Kassiererin kassiererin)
+        {
+            int reihe = kassiererin.frageNachReihe();
+            int sitz = kassiererin.frageNachSitz();
+
+            if (kinosaal.platzIstFrei(reihe, sitz))
+            {
+                kinosaal.belegePlatz(reihe, sitz);
+                kassiererin.bestaetigeReservierung(reihe, sitz);
+            }
+            else
+            {
+                kassiererin.lehneReservierungAb(reihe, sitz);
+            }
+
+            kassiererin.frageNachFortsetzung();
+
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
+            {
+                neueReservierung(kinosaal, kassiererin);
+            }
+            else if (Console.ReadKey().Key == ConsoleKey.Spacebar)
+            {
+                kassiererin.verabschiedeKunden();
+                Environment.Exit(0);
+            }
+        }
+    }
+}
